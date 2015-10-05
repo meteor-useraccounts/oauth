156eef1864ffc45c4dc6d4affcac97cc54f4cd2b
Luca Mussi <ing.luca.mussi@gmail.com>
2015-10-05 22:28:20 +0200

    added English texts

---
+++ src/texts.js
@@ -0,0 +1,16 @@
+/* global
+    UserAccounts: false
+*/
+/* jshint -W106 */
+'use strict';
+
+_.extend(UserAccounts.texts, {
+  add: 'add %s',
+  connect_to: 'connect to %s',
+  disconnect_from: 'disconnect from %s',
+  log_in_with: 'log in with %s',
+  register_with: 'register with %s',
+  remove: 'remove %s',
+  sign_in_with: 'sign in with %s',
+  sign_up_with: 'sign up with %s',
+});
